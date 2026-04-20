dae22a6dd6f075f74110854f0c5469b4f7b74833
elyrin <elyrinmiller@gmail.com>
2026-04-20 22:11:04 +0300

    Update Clear-Windows.bat
    Add clear internet data usage

--- Clear-Windows.bat
+++ Clear-Windows.bat
@@ -30,3 +30,41 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compat
 
 :: windows defender
 del /s /q "C:\ProgramData\Microsoft\Windows Defender\Scans\History"
+
+@echo off
+
+set user_path=C:\Users\%USERNAME%
+
+:: recent
+del /s /q "%user_path%\Recent"
+
+:: crashdumps
+rmdir /s /q "%user_path%\AppData\Local\CrashRpt"
+rmdir /s /q "%user_path%\AppData\Local\CrashDumps"
+rmdir /s /q "%user_path%\AppData\Local\CrashReportClient"
+
+:: temp
+del /s /q "%user_path%\AppData\Local\Temp"
+del /s /q "C:\Windows\Temp"
+
+:: nvidia
+del /s /q "C:\ProgramData\NVIDIA Corporation\Drs\nvAppTimestamps"
+
+:: recycle
+rd /s /q C:\$Recycle.Bin
+rd /s /q D:\$Recycle.Bin
+
+:: prefetch
+del /s /q "C:\Windows\Prefetch"
+
+:: registry
+reg delete "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /f
+
+:: windows defender
+del /s /q "C:\ProgramData\Microsoft\Windows Defender\Scans\History"
+
+:: internet data usage
+net stop DPS
+del /f /s /q C:\Windows\System32\sru\*
+net start DPS
